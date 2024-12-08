bd5f9008c282314a403cd8b2229b2410dc8c72f0
RuiyangHu22 <rhu22@my.centennialcollege.ca>
2024-12-08 04:35:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to Database'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
 
