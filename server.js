1d2437fba14fd3d2c621871bfb2517693791f13e
RuiyangHu22 <rhu22@my.centennialcollege.ca>
2024-12-09 14:43:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -30,20 +30,13 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/auth', authRouter);
 app.use('/books',  booksRouter);
 
-app.use(express.static(path.join(__dirname, 'vite', 'dist')));
+app.use(express.static(path.join(__dirname, 'vite')));
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'vite', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'vite', 'index.html'));
 });
 
-app.get('*', (req, res) => {
-    const nonApiRoutes = ['/', '/login', '/register', '/books'];
-    if (!nonApiRoutes.some((route) => req.originalUrl.startsWith(route))) {
-        return res.status(404).send('Not found');
-    }
-    res.sendFile(path.join(__dirname, 'vite', 'dist', 'index.html'));
-});
 
 
 // Start Server
